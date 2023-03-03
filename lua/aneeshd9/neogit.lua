408b91427716f65d4e9d2adbefbeb8294a50e4a5
Aneesh Dandime <aneesh.dandime9@gmail.com>
2023-03-03 11:04:01 -0500

    Added custom icons. And modified neogit icons.

--- lua/aneeshd9/neogit.lua
+++ lua/aneeshd9/neogit.lua
@@ -3,4 +3,16 @@ if not status_ok then
   return
 end
 
-neogit.setup({})
+local status_ok, icons = pcall(require, 'user.icons')
+if not status_ok then
+  return
+end
+
+neogit.setup({
+  disable_hint = true,
+  signs = {
+    section = { icons.ui.ArrowClosed, icons.ui.ArrowOpen },
+    item = { icons.ui.ArrowClosed, icons.ui.ArrowOpen },
+    hunk = { icons.ui.ArrowClosed, icons.ui.ArrowOpen },
+  },
+})
