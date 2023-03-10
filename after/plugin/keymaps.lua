41eae30778b8c12ad4ce9de74954bc3cc1281765
Aneesh Dandime <aneesh.dandime9@gmail.com>
2023-03-10 12:16:34 -0500

    Formatting.

--- after/plugin/keymaps.lua
+++ after/plugin/keymaps.lua
@@ -3,59 +3,59 @@ local default_opts = { noremap = true, silent = true }
 local expr_opts = { noremap = true, expr = true, silent = true }
 
 -- Better window navigation
-keymap("n", "<C-h>", "<C-w>h", default_opts)
-keymap("n", "<C-j>", "<C-w>j", default_opts)
-keymap("n", "<C-k>", "<C-w>k", default_opts)
-keymap("n", "<C-l>", "<C-w>l", default_opts)
-keymap("n", "<C-tab>", "<c-6>", default_opts)
+keymap('n', '<C-h>', '<C-w>h', default_opts)
+keymap('n', '<C-j>', '<C-w>j', default_opts)
+keymap('n', '<C-k>', '<C-w>k', default_opts)
+keymap('n', '<C-l>', '<C-w>l', default_opts)
+keymap('n', '<C-tab>', '<c-6>', default_opts)
 
 -- Tabs --
-keymap("n", "<m-t>", ":tabnew %<cr>", default_opts)
-keymap("n", "<m-y>", ":tabclose<cr>", default_opts)
-keymap("n", "<m-\\>", ":tabonly<cr>", default_opts)
+keymap('n', '<m-t>', ':tabnew %<cr>', default_opts)
+keymap('n', '<m-y>', ':tabclose<cr>', default_opts)
+keymap('n', '<m-\\>', ':tabonly<cr>', default_opts)
 
 -- Better escape using jk in insert and terminal mode
--- keymap("i", "jk", "<ESC>", default_opts)
-keymap("t", "<ESC>", "<C-\\><C-n>", default_opts)
-keymap("t", "<C-h>", "<C-\\><C-n><C-w>h", default_opts)
-keymap("t", "<C-j>", "<C-\\><C-n><C-w>j", default_opts)
-keymap("t", "<C-k>", "<C-\\><C-n><C-w>k", default_opts)
-keymap("t", "<C-l>", "<C-\\><C-n><C-w>l", default_opts)
+-- keymap('i', 'jk', '<ESC>', default_opts)
+keymap('t', '<ESC>', '<C-\\><C-n>', default_opts)
+keymap('t', '<C-h>', '<C-\\><C-n><C-w>h', default_opts)
+keymap('t', '<C-j>', '<C-\\><C-n><C-w>j', default_opts)
+keymap('t', '<C-k>', '<C-\\><C-n><C-w>k', default_opts)
+keymap('t', '<C-l>', '<C-\\><C-n><C-w>l', default_opts)
 
 -- Center search results
-keymap("n", "n", "nzzzv", default_opts)
-keymap("n", "N", "Nzzzv", default_opts)
-keymap("n", "g,", "g,zvzz", default_opts)
-keymap("n", "g;", "g;zvzz", default_opts)
+keymap('n', 'n', 'nzzzv', default_opts)
+keymap('n', 'N', 'Nzzzv', default_opts)
+keymap('n', 'g,', 'g,zvzz', default_opts)
+keymap('n', 'g;', 'g;zvzz', default_opts)
 
 -- Scrolling
-keymap("n", "<C-d>", "<C-d>zz", default_opts)
-keymap("n", "<C-u>", "<C-u>zz", default_opts)
+keymap('n', '<C-d>', '<C-d>zz', default_opts)
+keymap('n', '<C-u>', '<C-u>zz', default_opts)
 
 -- Visual line wraps
-keymap("n", "k", "v:count == 0 ? 'gk' : 'k'", expr_opts)
-keymap("n", "j", "v:count == 0 ? 'gj' : 'j'", expr_opts)
+keymap('n', 'k', 'v:count == 0 ? \'gk\' : \'k\'', expr_opts)
+keymap('n', 'j', 'v:count == 0 ? \'gj\' : \'j\'', expr_opts)
 
 -- Better indent
-keymap("v", "<", "<gv", default_opts)
-keymap("v", ">", ">gv", default_opts)
+keymap('v', '<', '<gv', default_opts)
+keymap('v', '>', '>gv', default_opts)
 
 -- Paste over currently selected text without yanking it
-keymap("v", "p", '"_dP', default_opts)
+keymap('v', 'p', '"_dP', default_opts)
 
 -- Cancel search highlighting with ESC
-keymap("n", "<ESC>", ":nohlsearch<Bar>:echo<CR>", default_opts)
+keymap('n', '<ESC>', ':nohlsearch<Bar>:echo<CR>', default_opts)
 
 -- Move selected line / block of text in visual mode
-keymap("x", "K", ":move '<-2<CR>gv=gv", default_opts)
-keymap("x", "J", ":move '>+1<CR>gv=gv", default_opts)
+keymap('x', 'K', ':move \'<-2<CR>gv=gv', default_opts)
+keymap('x', 'J', ':move \'>+1<CR>gv=gv', default_opts)
 
 -- Resizing panes
-keymap("n", "<Left>", ":vertical resize +1<CR>", default_opts)
-keymap("n", "<Right>", ":vertical resize -1<CR>", default_opts)
-keymap("n", "<Up>", ":resize -1<CR>", default_opts)
-keymap("n", "<Down>", ":resize +1<CR>", default_opts)
+keymap('n', '<Left>', ':vertical resize +1<CR>', default_opts)
+keymap('n', '<Right>', ':vertical resize -1<CR>', default_opts)
+keymap('n', '<Up>', ':resize -1<CR>', default_opts)
+keymap('n', '<Down>', ':resize +1<CR>', default_opts)
 
 -- Insert blank line
-keymap("n", "]<Space>", "o<Esc>", default_opts)
-keymap("n", "[<Space>", "O<Esc>", default_opts)
+keymap('n', ']<Space>', 'o<Esc>', default_opts)
+keymap('n', '[<Space>', 'O<Esc>', default_opts)
